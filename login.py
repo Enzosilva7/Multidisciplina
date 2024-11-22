7c6c8bc34390f2c618fd94ab6a16d719bfa9c3ba
JoaoNunes966 <168840048+JoaoNunes966@users.noreply.github.com>
2024-11-22 16:09:12 -0300

    Códigos Login
    códigos de login conceitual para apresentação do trabalho multidisciplinar.

---
+++ login.py
@@ -0,0 +1,11 @@
+import time
+
+user = input("digite seu login")
+
+if user == "RafexCabelux07":
+    print("Fazendo verificação de segurança...")
+    time.sleep(1)
+    print("Login realizado com suceso!!")
+else:
+    print("Login não encontrado")
+input()
